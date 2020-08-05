7e2bc5dcfd8432cb7e6e396bbd2fab4f33222ef9
ChauhanSiddharth <siddharth.chauhan50@yahoo.com>
2020-08-05 17:45:49 -0400

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+from locust import HttpUser, between, task
+class WebsiteUser(HttpUser):
+    wait_time = between(5, 15)
+    
+    @task
+    def index(self):
+        self.client.get("/")
+        
+    @task
+    def about(self):
+        self.client.get("/about")  
